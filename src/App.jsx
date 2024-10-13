412252962aea80cd71dc1a9601d017804a3d9965
emrd434 <emrd434@gmail.com>
2024-10-13 10:04:03 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <a href="ms-settings:windowsupdate">
+        <button>Open App</button>
+      </a>
+    </div>
+  );
+}
+
+export default App;
